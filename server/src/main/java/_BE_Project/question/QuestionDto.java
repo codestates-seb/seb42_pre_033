e0e6df8d32d15304c93bafd42ad39dd7b0ec5e49
KBM <kimbm1114@gmail.com>
2023-02-17 11:12:19 +0900

    🔧 add question(v1)

--- server/src/main/java/_BE_Project/question/QuestionDto.java
+++ server/src/main/java/_BE_Project/question/QuestionDto.java
@@ -9,18 +9,24 @@ public class QuestionDto {
     @AllArgsConstructor
     @Getter
     public static class Post {
-
+        private long questionId;
+        private String title;
+        private String content;
     }
 
     @Getter
     @Setter
     public static class Patch {
-
+        private long questionId;
+        private String title;
+        private String content;
     }
 
     @AllArgsConstructor
     @Getter
     public static class Response {
-
+        private long questionId;
+        private String title;
+        private String content;
     }
 }
