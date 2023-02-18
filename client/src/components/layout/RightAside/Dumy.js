da14439483dd72f022fd0151ad1bd8c15dde3619
Jung Su <wjdwjdtn92@naver.com>
2023-02-18 20:19:16 +0900

    🔨 aside 더미데이터 추가

---
+++ client/src/components/layout/RightAside/Dumy.js
@@ -0,0 +1,73 @@
+export const DUMY = [
+  {
+    title: 'The Overflow Blog',
+    contentList: [
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content:
+          'Monitoring debt builds up faster than software teams can pay it off',
+        subcontent: '',
+      },
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content:
+          'Because the only thing worse than building internal tools is maintaining them...',
+        subcontent: '',
+      },
+    ],
+  },
+  {
+    title: 'Featured on Meta',
+    contentList: [
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content: 'Ticket smash for [status-review] tag: Part Deux',
+        subcontent: '',
+      },
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content: `We've added a "Necessary cookies only" option to the cookie consent popup`,
+        subcontent: '',
+      },
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content: 'We’ve made changes to our Privacy Notice for Collectives™',
+        subcontent: '',
+      },
+      {
+        icon: '../../../assets/icons/pencil.svg',
+        content: 'Microsoft Azure Collective launch and proposed tag changes',
+        subcontent: '',
+      },
+    ],
+  },
+];
+
+export const DUMY2 = [
+  {
+    title: 'Colletive',
+    companyList: [
+      {
+        icon: 'https://cdn.sstatic.net/Sites/stackoverflow/Img/subcommunities/google-cloud.svg?v=5d10e4a96c5b',
+        company: 'Microsoft Azure',
+        descrition:
+          'Google Cloud provides organizations with leading infrastructure, platform capabilities and industry solutions to help them solve',
+        members: '37k',
+      },
+      {
+        icon: '	https://cdn.sstatic.net/Sites/stackoverflow/Img/subcommunities/twilio.svg?v=ab07490da974',
+        company: 'Twilio',
+        descrition:
+          'Twilio has democratized channels like voice, text, chat, video, and email by virtualizing',
+        members: '7k',
+      },
+      {
+        icon: 'https://cdn.sstatic.net/Sites/stackoverflow/Img/subcommunities/aws.svg?v=46d86cbff922',
+        company: 'AWS',
+        descrition:
+          'Amazon Web Services (AWS) is the world’s most comprehensive and broadly adopted cloud platform, offering over 200 fully',
+        members: '7k',
+      },
+    ],
+  },
+];
