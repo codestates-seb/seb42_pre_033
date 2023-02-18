6d318f6b581361bcbd00fad113204ca977addcfe
Jung Su <wjdwjdtn92@naver.com>
2023-02-18 20:23:14 +0900

    ✨ 구현된 레이아웃 확인용 page 추가

---
+++ client/src/pages/LayoutTest.js
@@ -0,0 +1,50 @@
+import { DUMY, DUMY2 } from '../components/layout/RightAside/Dumy';
+import RightAside from '../components/layout/RightAside/RightAside';
+import RightAsideColleiveItem from '../components/layout/RightAside/RightAsideColleiveItem';
+import RightAsideItem from '../components/layout/RightAside/RightAsideItem';
+import Button from '../components/UI/Button';
+
+function LayoutTest() {
+  return (
+    <div>
+      {DUMY.map(({ title, contentList }) => (
+        <RightAside key={title} color='yellow' title={title}>
+          {contentList.map(({ icon, content, subcontent }) => (
+            <RightAsideItem
+              key={content}
+              icon={icon}
+              content={content}
+              subcontent={subcontent}
+            />
+          ))}
+        </RightAside>
+      ))}
+
+      {DUMY2.map(({ title, companyList }) => (
+        <RightAside
+          key={title}
+          color='gray'
+          title={title}
+          headbuttonText='see all'
+        >
+          {companyList.map(({ icon, company, descrition, members }) => (
+            <RightAsideColleiveItem
+              key={company}
+              icon={icon}
+              company={company}
+              descrition={descrition}
+              members={members}
+            />
+          ))}
+        </RightAside>
+      ))}
+
+      <Button variant='primary'> primary</Button>
+      <Button variant='secondary'> secondary</Button>
+      <Button variant='tertiary'> tertiary</Button>
+      <Button variant='text'> text</Button>
+    </div>
+  );
+}
+
+export default LayoutTest;
