bd5d20e98b9b444b13ecf83cd7bdc0d5bdf72d18
Jung Su <wjdwjdtn92@naver.com>
2023-02-20 16:54:55 +0900

    ✨ 회원가입 bottom 추가

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,9 +1,31 @@
-import SignupForm from '../components/SignupForm/SignupForm';
+import styled from 'styled-components';
+import SignupBottom from '../components/Signup/SignupBottom';
+import SignupForm from '../components/Signup/SignupForm';
+import SignupHeader from '../components/Signup/SignupHeader';
+
+const Container = styled.section`
+  display: flex;
+  gap: 48px;
+`;
+
+const SignupFormContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
 
 function SignupPage() {
   const onSubmit = () => {};
 
-  return <SignupForm onSubmit={onSubmit} />;
+  return (
+    <Container>
+      <SignupHeader />
+      <SignupFormContainer>
+        <SignupForm onSubmit={onSubmit} />
+        <SignupBottom />
+      </SignupFormContainer>
+    </Container>
+  );
 }
 
 export default SignupPage;
