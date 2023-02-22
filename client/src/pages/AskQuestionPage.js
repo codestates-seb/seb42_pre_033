a66157d49d9342bab160f43d10a06695fd5a7e77
Jung Su <wjdwjdtn92@naver.com>
2023-02-23 04:36:10 +0900

    🔨질문하기 화면 Footer 추가

--- client/src/pages/AskQuestionPage.js
+++ client/src/pages/AskQuestionPage.js
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
 import AskQuestion from '../components/AskQuestion/AskQuestion';
 import Main from '../components/layout/Main/Main';
+import Footer from '../components/layout/Footer';
 
 const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 32px;
 `;
 
 function AskQuestionPage() {
   return (
-    <Main>
-      <Wrapper>
-        <AskQuestion />
-      </Wrapper>
-    </Main>
+    <>
+      <Main>
+        <Wrapper>
+          <AskQuestion />
+        </Wrapper>
+      </Main>
+      <Footer />
+    </>
   );
 }
 
