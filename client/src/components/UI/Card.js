36dca3fd8433ac924ab6cab2ef314ad104d55c42
Jung Su <wjdwjdtn92@naver.com>
2023-02-23 04:27:56 +0900

    🔨 공톰 컴포넌트 card 타입 추가

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -1,15 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const VARIANTS = {
+  primary: css`
+    --card-border-color: var(--white);
+  `,
+  secondary: css`
+    --card-border-color: var(--black-075);
+  `,
+};
 
 export const StyledCard = styled.div`
+  ${({ variant }) => VARIANTS[variant]}
+
   border-radius: 7px;
   box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
     0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
   padding: 24px;
   background-color: var(--white);
+  border: 1px solid var(--card-border-color);
 `;
 
-function Card({ children, ...props }) {
-  return <StyledCard {...props}>{children}</StyledCard>;
+function Card({ variant = 'primary', children, ...props }) {
+  return (
+    <StyledCard variant={variant} {...props}>
+      {children}
+    </StyledCard>
+  );
 }
 
 export default Card;
