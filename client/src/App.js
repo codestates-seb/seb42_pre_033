0cb8423a198c69a713f75d52e88bf4a291b6ce79
Jung Su <wjdwjdtn92@naver.com>
2023-02-23 10:00:55 +0900

    ♻️ router 컴포넌트 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Header from './components/layout/Header';
-import Main from './components/layout/Main/Main';
-import UserEdit from './components/User/UserEdit/UserEdit';
-import UserProfile from './components/User/UserProfile/UserProfile';
-import UserDelete from './components/User/UserDelete/UserDelete';
+
+import Router from './Router';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Router />
       <GlobalStyles />
-      <Routes>
-        <Route path='/' element={<Main />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/users/profile' element={<UserProfile />}></Route>
-        <Route path='/users/edit' element={<UserEdit />}></Route>
-        <Route path='/users/Delete' element={<UserDelete />}></Route>
-      </Routes>
     </BrowserRouter>
   );
 }
