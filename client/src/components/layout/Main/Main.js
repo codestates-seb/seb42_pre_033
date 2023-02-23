69487fe2315d69e5aff5d477fb1856669e8ae6dc
Jung Su <wjdwjdtn92@naver.com>
2023-02-23 09:51:37 +0900

    ♻️ 공통 레이아웃 수정

--- client/src/components/layout/Main/Main.js
+++ client/src/components/layout/Main/Main.js
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 //height 임의 설정
 const MainComponent = styled.main`
   width: 100%;
-  min-width: 800px;
-  min-height: calc(100vh - 50px);
-  max-width: 1264px;
+  min-height: 100%;
   margin: 0 auto;
   display: flex;
   margin-bottom: 16px;
