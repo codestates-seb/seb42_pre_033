1dc6030d435890fe3be4e1431ef59a4fc76cd805
parkjinsun <gd5177@naver.com>
2023-02-23 22:19:54 +0900

    pull 에러 해결중

--- server/src/main/java/_BE_Project/member/service/MemberService.java
+++ server/src/main/java/_BE_Project/member/service/MemberService.java
@@ -7,14 +7,10 @@ import _BE_Project.member.repository.MemberRepository;
 import _BE_Project.member.repository.RefreshTokenRedisRepository;
 import _BE_Project.security.utils.CustomAuthorityUtils;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.springframework.security.core.context.SecurityContextHolder;
-=======
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
->>>>>>> 36d5763036450354cc4ad6e0095cf24646a4eae8
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,13 +25,8 @@ public class MemberService {
   private final MemberRepository memberRepository;
   private final PasswordEncoder passwordEncoder;
   private final CustomAuthorityUtils authorityUtils;
-<<<<<<< HEAD
   private final RefreshTokenRedisRepository redisRepository;
-  
-=======
 
-
->>>>>>> 36d5763036450354cc4ad6e0095cf24646a4eae8
   public Member saveMember(Member member){
     Member findMember = memberRepository.findByEmail(member.getEmail());
     member.setPassword(passwordEncoder.encode(member.getPassword()));
@@ -50,8 +41,6 @@ public class MemberService {
 
     return memberRepository.save(findMember);
   }
-  
-<<<<<<< HEAD
   public void logout(HttpServletRequest request){
     String accessToken = request.getHeader("Authorization").substring(7);
     String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -61,8 +50,6 @@ public class MemberService {
     redisRepository.deleteBy(email);
   }
   
-  public void verifyExistsEmail(String email){
-=======
   @Transactional(readOnly = true)
   public Member findMember(long memberId){
     Member findMember = memberRepository.findById(memberId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
@@ -78,12 +65,4 @@ public class MemberService {
     Member findMember = memberRepository.findByMemberId(memberId);
     memberRepository.delete(findMember);
   }
-  
-  /*public void verifyExistsEmail(String email){
->>>>>>> 36d5763036450354cc4ad6e0095cf24646a4eae8
-    Optional<Member> optionalMember = memberRepository.findByEmail(email);
-    if(optionalMember.isPresent()){
-      throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
-    }
-  }*/
 }
