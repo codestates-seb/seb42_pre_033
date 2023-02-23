9c6b9e4737a9fa50011e5b56a6e7cd65d67054e4
Jung Su <wjdwjdtn92@naver.com>
2023-02-23 14:21:53 +0900

    🔨 warning type 추가

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -2,10 +2,17 @@ import styled, { css } from 'styled-components';
 
 const VARIANTS = {
   primary: css`
+    --card-bg-color: var(--white);
     --card-border-color: var(--white);
   `,
   secondary: css`
+    --card-bg-color: var(--white);
     --card-border-color: var(--black-075);
+  `,
+
+  warning: css`
+    --card-bg-color: var(--yellow-050);
+    --card-border-color: var(--yellow-300);
   `,
 };
 
@@ -16,7 +23,7 @@ export const StyledCard = styled.div`
   box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
     0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
   padding: 24px;
-  background-color: var(--white);
+  background-color: var(--card-bg-color);
   border: 1px solid var(--card-border-color);
 `;
 
