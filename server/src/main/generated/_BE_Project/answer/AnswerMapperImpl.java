979ccc76d2f4a5b60422eac2e7d3ec1b810be799
kim <whdrb335@gmail.com>
2023-02-26 16:38:04 +0900

    ww

---
+++ server/src/main/generated/_BE_Project/answer/AnswerMapperImpl.java
@@ -0,0 +1,39 @@
+package _BE_Project.answer;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-26T16:02:18+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class AnswerMapperImpl implements AnswerMapper {
+
+    @Override
+    public Answer answerPostDtoAnswer(AnswerDto.Post answerPostDto) {
+        if ( answerPostDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setAnswerContent( answerPostDto.getAnswerContent() );
+
+        return answer;
+    }
+
+    @Override
+    public Answer answerPatchDtoAnswer(AnswerDto.Patch answerPatchDto) {
+        if ( answerPatchDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setAnswerContent( answerPatchDto.getAnswerContent() );
+
+        return answer;
+    }
+}
