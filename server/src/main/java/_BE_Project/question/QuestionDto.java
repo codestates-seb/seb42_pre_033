e688919d31d576f8769994f80a9409bb63a7efb0
KBM <kimbm1114@gmail.com>
2023-02-27 12:27:06 +0900

    📝 quesiton 조회로직 추가

--- server/src/main/java/_BE_Project/question/QuestionDto.java
+++ server/src/main/java/_BE_Project/question/QuestionDto.java
@@ -1,11 +1,14 @@
 package _BE_Project.question;
 
+import _BE_Project.answer.Answer;
+import _BE_Project.answer.AnswerDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class QuestionDto {
 
@@ -29,6 +32,7 @@ public class QuestionDto {
     @Getter
     @Setter
     public static class Response {
+        private long memberId;
         private long questionId;
         private String title;
         private String content;
@@ -37,5 +41,7 @@ public class QuestionDto {
         private int score;
         //조회수
         private int viewCnt;
+        //질문에 해당하는 답변
+        private List<AnswerDto.Response> answers;
     }
 }
