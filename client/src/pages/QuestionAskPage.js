6d92d6304cb082c63459397ea8b59dfd43607941
Jung Su <wjdwjdtn92@naver.com>
2023-02-28 17:20:02 +0900

    🔨 서버 데이터 요청 추가 및 props 서버 응답과 동일하게 변경

--- client/src/pages/QuestionAskPage.js
+++ client/src/pages/QuestionAskPage.js
@@ -1,7 +1,10 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import QuestionAskForm from '../components/QuestionAsk/QuestionAskForm';
 import QuestionAskHeadr from '../components/QuestionAsk/QuestionAskHeadr';
 import AskQuetionNotice from '../components/QuestionAsk/QuestionAskNotice';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Wrapper = styled.section`
   display: flex;
@@ -21,8 +24,48 @@ const Container = styled.article`
 `;
 
 function QuestionAskPage() {
-  const handleSumbit = ({ title, body }) => {
-    console.log(title, body);
+  const { accessToken, refreshToken } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleSumbit = ({ title, content }) => {
+    if (!title || !content) {
+      return;
+    }
+    axios({
+      method: 'post',
+      url: '/api/questions',
+      data: {
+        title: title,
+        content: content.replaceAll('<', '&lt;').replaceAll('>', '&gt;'),
+      },
+      headers: {
+        authorization: accessToken,
+        refresh: refreshToken,
+      },
+    })
+      .then((response) => {
+        if (response.status === 409) {
+          // error coming back from server
+          console.log(response.message);
+          return;
+        }
+
+        if (response.status === 400) {
+          console.log('입력값 오류');
+          return;
+        }
+
+        if (response.status !== 201) {
+          console.log('서버 오류');
+          return;
+        }
+
+        console.log(response.data);
+        navigate(`/question/${response.data.data}`);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   };
 
   return (
