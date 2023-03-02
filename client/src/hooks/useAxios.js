51360ba822093143cd58fc32c848e090e432c535
Jung Su <wjdwjdtn92@naver.com>
2023-03-02 14:24:52 +0900

    ✅ 배포 오류 확인을 위해 로그 추가

--- client/src/hooks/useAxios.js
+++ client/src/hooks/useAxios.js
@@ -28,6 +28,7 @@ function useAxios({ method = 'get', url, headers, body, auth, params }) {
         setloading(false);
       })
       .catch((error) => {
+        console.log(error);
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
