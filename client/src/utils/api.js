73b521779072d80ade3491ae9b4359813055b955
Jung Su <wjdwjdtn92@naver.com>
2023-03-02 13:34:00 +0900

    🔨 postAnswer, getAnswer, patchQuestion 추가

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -43,3 +43,67 @@ export async function postQuestion({
 
   return { data, status };
 }
+
+export async function postAnswer({
+  questionId,
+  content,
+  accessToken,
+  refreshToken,
+}) {
+  return await axios({
+    method: 'post',
+    url: `${BASE_URL}/answers/${questionId}`,
+    data: {
+      answerContent: content,
+    },
+    headers: {
+      authorization: accessToken,
+      refresh: refreshToken,
+    },
+  })
+    .then(({ data, status }) => {
+      return { data, status };
+    })
+    .catch(({ response, ...rest }) => {
+      console.log(rest);
+      return { data: response.data, status: response.status };
+    });
+}
+
+export async function getAnswer(questionId) {
+  return await axios({
+    method: 'get',
+    url: `${BASE_URL}/questions/${questionId}`,
+    headers: 'ngrok-skip-browser-warning: "12"',
+  })
+    .then(({ data, status }) => {
+      return { data, status };
+    })
+    .catch(({ response }) => {
+      return { data: response.data, status: response.status };
+    });
+}
+
+export async function patchQuestion({
+  title,
+  content,
+  questionId,
+  accessToken,
+  refreshToken,
+}) {
+  return await axios({
+    method: 'patch',
+    url: `${BASE_URL}/questions/${questionId}`,
+    data: { title, content },
+    headers: {
+      authorization: accessToken,
+      refresh: refreshToken,
+    },
+  })
+    .then(({ data, status }) => {
+      return { data, status };
+    })
+    .catch(({ response }) => {
+      return { data: response.data, status: response.status };
+    });
+}
